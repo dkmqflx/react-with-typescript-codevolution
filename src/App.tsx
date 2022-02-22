143658fda609f7df2af47682e8da25ab8c45a342
dkmqflx <dkmqflx@gmail.com>
2022-02-22 23:05:38 +0900

    3,4 - Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
 import React from 'react';
+import { Greet } from './components/Greet';
 import './App.css';
+import { Person } from './components/Person';
+import { PersonList } from './components/PersonList';
 
 function App() {
-  return <div className="App"></div>;
+  const personName = {
+    first: 'Bruce',
+    last: 'Wayne',
+  };
+
+  const nameList = [
+    {
+      first: 'Bruce',
+      last: 'Wayne',
+    },
+    {
+      first: 'Bruce2',
+      last: 'Wayne2',
+    },
+    {
+      first: 'Bruce3',
+      last: 'Wayne3',
+    },
+  ];
+
+  return (
+    <div className="App">
+      <Greet name="dododo" messageCount={10} isLoggedIn={false}></Greet>
+      <Person name={personName}></Person>
+      <PersonList names={nameList}></PersonList>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Greet.tsx
@@ -0,0 +1,20 @@
+type GreetProps = {
+  name: string;
+  messageCount: number;
+  isLoggedIn: boolean;
+};
+
+// 그냥 props 만 적으면 에러
+// {name: stirng} 형태가 넘어오기 때문에
+// props:string이 앙니라, 위처럼 타입을 정의해주어야 한다.
+
+export function Greet(props: GreetProps) {
+  return (
+    <div>
+      {props.isLoggedIn
+        ? ` This ${props.name} is Greet ${props.messageCount}
+      state is`
+        : 'not loggged'}
+    </div>
+  );
+}
